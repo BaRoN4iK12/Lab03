dc149beaca59cf2867f7856079531ef2ddd73136
Baron4ik <baron23803@yandex.ru>
2024-09-10 23:47:24 +0300

    Добавьте файлы проекта.

---
+++ Source.c
@@ -0,0 +1,25 @@
+#include <locale.h>
+
+#include <stdio.h>
+
+void main()
+
+{
+
+    setlocale(LC_ALL, "RUS");
+
+    puts("  ,-.       _,---._ __  / \\");
+    puts(" /  )    .-'       `./ /   \\" );
+    puts("(  (   ,'            `/    /|");
+    puts(" \\  `-'             \\'\\   / |");
+    puts("  `.              ,  \\ \\ /  |");
+    puts("   /`.          ,'-`----Y   |");
+    puts("  (            ;        |   '");
+    puts("  |  ,-.    ,-'         |  /");
+    puts("  |  | (   |            | /");
+    puts("  )  |  \\  `.___________|/");
+    puts("  `--'   `--'");
+
+	getchar();
+
+}
